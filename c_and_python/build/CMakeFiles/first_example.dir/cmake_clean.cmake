b96fcdc0e455b339eaec20d5b72f237f0f4da6e0
Sangeeta <shongmusic@gmail.com>
2019-10-16 22:13:03 +0530

    Starting

---
+++ c_and_python/build/CMakeFiles/first_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_example.dir/first_example.cpp.o"
+  "first_example.pdb"
+  "first_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
